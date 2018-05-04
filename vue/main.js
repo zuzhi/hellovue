71f8692470932556d690a7efa3d71c2ee4d0c815
zuzhi <476513451@qq.com>
2018-05-04 16:39:57 +0800

    Learn Named Slots

--- vue/main.js
+++ vue/main.js
@@ -133,12 +133,38 @@ Vue.component('coupon2', {
 
 })
 
+// modal card
+Vue.component('modal2', {
+    template: `
+    <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-card">
+            <header class="modal-card-head">
+                <p class="modal-card-title">
+                    <slot name="header">Default header</slot>
+                </p>
+                <button class="delete" aria-label="close" @click="$emit('close')"></button>
+            </header>
+            <section class="modal-card-body">
+                <slot></slot>
+            </section>
+            <footer class="modal-card-foot">
+                <slot name="footer">
+                    <button class="button is-success" @click="$emit('close')">OK</button>
+                </slot>
+            </footer>
+        </div>
+    </div>
+    `
+})
+
 let app = new Vue({
     el: '#root',
     data: {
         showModal: false,
         couponApplied: false,
-        coupon2Applied: false
+        coupon2Applied: false,
+        showModal2: false
     },
     methods: {
         onCouponApplied() {
