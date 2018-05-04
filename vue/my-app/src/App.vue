f8b9d67b90cb7da007cbe7a2eaf62247fd7869d6
zuzhi <476513451@qq.com>
2018-05-04 21:12:40 +0800

    Learn Webpack and Vue CLI

---
+++ vue/my-app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <message>hello</message>
+    <message>hello (again)</message>
+  </div>
+</template>
+
+<script>
+import Message from './components/Message.vue'
+
+export default {
+  name: 'app',
+  components: { Message },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
