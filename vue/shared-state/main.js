e6b19ddfef3e0a3c49ed5d695213ace970024270
zuzhi <476513451@qq.com>
2018-05-17 10:34:13 +0800

    Learn Shared State 101

---
+++ vue/shared-state/main.js
@@ -0,0 +1,19 @@
+let store = {
+    user: {
+        name: 'John Doe'
+    }
+}
+
+new Vue({
+    el: '#one',
+    data: {
+        shared: store
+    }
+})
+
+new Vue({
+    el: '#two',
+    data: {
+        shared: store
+    }
+})
